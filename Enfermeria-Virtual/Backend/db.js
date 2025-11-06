e632c2023bb6cd19cdb6c87897ba81567dd62cab
robertomontoya01 <roberto.montoya4734@alumnos.udg.mx>
2025-11-05 19:10:40 -0600

    Correccion de conexion

--- Enfermeria-Virtual/Backend/db.js
+++ Enfermeria-Virtual/Backend/db.js
@@ -2,12 +2,14 @@ require("dotenv").config();
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "root",
-  database: process.env.DB_NAME || "enfermeria_virtual",
-  port: process.env.DB_PORT || 3306,
-  ssl: process.env.DB_SSL === "true" ? { rejectUnauthorized: true } : undefined,
+  host: process.env.MYSQLHOST || "localhost",
+  user: process.env.MYSQLUSER || "root",
+  password: process.env.MYSQLPASSWORD || "root",
+  database: process.env.MYSQLDATABASE || "enfermeria_virtual",
+  port: process.env.MYSQLPORT || 3306,
+  ssl: {
+    rejectUnauthorized: true,
+  },
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
