25f40246fd442c043145f0c67cbb89c16c7c20dc
robertomontoya01 <roberto.montoya4734@alumnos.udg.mx>
2025-11-05 21:10:54 -0600

    Cambios en el front

--- Enfermeria-Virtual/Frontend/constants/config.ts
+++ Enfermeria-Virtual/Frontend/constants/config.ts
@@ -1,1 +1,6 @@
-export const API_BASE_URL = "http://10.0.2.2:3000";
+export const ENV = process.env.EXPO_PUBLIC_ENV ?? "development";
+export const API_BASE_URL =
+  process.env.EXPO_PUBLIC_API_BASE_URL ?? "http://10.0.2.2:3000";
+
+console.log(`🌎 Entorno activo: ${ENV}`);
+console.log(`🔗 API: ${API_BASE_URL}`);
