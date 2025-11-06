a4fd59c13ffc8b28bf1767a269af42480aaf683f
robertomontoya01 <roberto.montoya4734@alumnos.udg.mx>
2025-11-05 19:30:46 -0600

    Importacion de BD

--- Enfermeria-Virtual/Backend/db.js
+++ Enfermeria-Virtual/Backend/db.js
@@ -2,14 +2,12 @@ require("dotenv").config();
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: process.env.MYSQLHOST || "localhost",
-  user: process.env.MYSQLUSER || "root",
-  password: process.env.MYSQLPASSWORD || "root",
-  database: process.env.MYSQLDATABASE || "enfermeria_virtual",
-  port: process.env.MYSQLPORT || 3306,
-  ssl: {
-    rejectUnauthorized: true,
-  },
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
+  ssl: { rejectUnauthorized: true },
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
